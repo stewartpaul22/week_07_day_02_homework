732fa9f8a9c84b4a149bf39ef1b84db9565ff6ce
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-06 21:46:06 +0000

    Add Person abstract class

---
+++ src/main/java/Person.java
@@ -0,0 +1,12 @@
+public abstract class Person {
+
+    private String name;
+    private int age;
+    private int height;
+
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+}
