d79b3f65a443668634993678b86d0e8cca31c81a
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-06 22:06:19 +0000

    Add tests to StaffTest for getters

--- src/test/java/StaffTest.java
+++ src/test/java/StaffTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class StaffTest {
 
@@ -7,5 +10,20 @@ public class StaffTest {
     @Before
     public void setUp() throws Exception {
         staff = new Staff("Bill", 35, 180);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Bill", staff.getName());
+    }
+
+    @Test
+    public void hasAge() {
+        assertEquals(35, staff.getAge());
+    }
+
+    @Test
+    public void hasHeight() {
+        assertEquals(180, staff.getHeight());
     }
 }
